564b6a7c3851aaf6149fe67c5b34c541e876b2c3
Mayesha-Tasnim <mayesatasnim68@gmail.com>
2023-01-01 17:05:51 +0600

    changes have been made

--- js/script.js
+++ js/script.js
@@ -12,3 +12,13 @@ cross.addEventListener("click", function () {
   sideNav.classList.remove("active");
   overlay.classList.remove("overlayActive");
 });
+
+/**slide left side nav**/
+const slideLeftBtn = document.querySelector(".slide-left");
+const sideNav = document.querySelector(".side-nav");
+const mainSec = document.querySelector(".main");
+slideLeftBtn.addEventListener("click", function () {
+  sideNav.classList.toggle("slide-side-nav");
+  slideLeftBtn.classList.toggle("slide-right");
+  mainSec.classList.toggle("main-slide-left");
+});
